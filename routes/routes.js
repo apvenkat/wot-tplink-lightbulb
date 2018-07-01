161d3d687fc7631dba1f57fb81dbbe9e5bf6f2e8
apvenkat <venkat.subramanian94@gmail.com>
2018-07-01 15:23:10 +0800

    Console logging for all the controls added

--- routes/routes.js
+++ routes/routes.js
@@ -14,12 +14,18 @@ router.get('/', function (req, res) {
 })
 
 router.post('/on', function (req, res) {
-  light.power(1);
+  light.power(1).then(response => {
+    console.log(response)
+  })
+  .catch(e => console.error(e))
   return res.sendFile(path.join(__dirname +'./../views/control.html'));
 
 })
 router.post('/off', function (req, res) {
-  light.power(0);
+  light.power(0).then(response => {
+    console.log(response)
+  })
+  .catch(e => console.error(e))
   return res.sendFile(path.join(__dirname +'./../views/control.html'));
 
 })
@@ -30,9 +36,14 @@ router.get('/on/:value', function (req, res) {
       'transition_light_state': {
         'on_off': 1,
         'brightness':JSON.parse(req.params.value)
-      }
-  }})
 
+      }
+
+}})
+.then(response => {
+  console.log(response)
+})
+.catch(e => console.error(e))
   return res.sendFile(path.join(__dirname +'./../views/control.html'));
 
 
